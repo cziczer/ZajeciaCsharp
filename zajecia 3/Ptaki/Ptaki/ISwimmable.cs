c95bd1356011a8862e962a960a2f68f28e37854e
Mateusz Mastalerczyk <mmastalerczyk@gmail.com>
2019-11-23 14:06:53 +0100

    Add files via upload

---
+++ zajecia 3/Ptaki/Ptaki/ISwimmable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ptaki
+{
+    public interface IRun   
+    {
+        void Run();
+    }
+
+    public interface ISwimmable
+    {
+        int Weight { get; }
+
+        void Swim();
+    }
+
+    public interface IFlyable
+    {
+        int Weight { get; }
+
+        void Fly();
+    }
+
+    public interface INoFlyable
+    {
+        int Weight { get; }
+
+        void CantFly();
+    }
+}
---
+++ zajecia 3/Ptaki/Ptaki/Ptak.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace Ptaki
+{
+    class Ptak : IRun
+    {
+        public int Speed { get; set; }
+
+        public void Hello()
+        {
+            Console.WriteLine("Cześć jestem ptakiem!");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Run");
+        }
+    }
+
+    class Eagle : Ptak, IFlyable
+    {
+        public void Hello() 
+        {
+            Console.WriteLine("Jestem orłem, latam szybko");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("AAAAAAAAAA");
+        }
+
+        public int Weight { get; private set; }
+        public void Fly()
+        {
+            Console.WriteLine("Lece sobie ");
+        }
+    }
+
+    class Penguin : Ptak, ISwimmable, INoFlyable
+    {
+        public void CantFly()
+        {
+            Console.WriteLine("Sorry, nie umiem latać:/'");
+        }
+
+        public int Weight { get; }
+
+        public void Swim()
+        {
+            Console.WriteLine("Jestem pingwinem, nie umiem latać ale szybko pływam");
+        }
+    }
+
+    class Emu : INoFlyable
+    {
+        public int Weight { get; }
+        public void CantFly()
+        {
+            Console.WriteLine("Też nie umiem latać, ale szybko biegam");
+        }
+    }
+    class Duck : Ptak, IFlyable
+    {
+        public int Weight { get; }
+
+        public void Fly()
+        {
+            Console.WriteLine("Jestem kaczką, latam");
+        }
+    }
+
+   
+}
